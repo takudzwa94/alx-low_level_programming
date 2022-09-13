56a3d0dfc4d69eedf721e9cd4b911293d442e15e
takudzwa94 <51080246+takudzwa94@users.noreply.github.com>
2022-09-13 12:38:45 +0200

    Update and rename 0-holberton.c to 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "putchar.h"
 /**
  * main - Entry point
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char *sh = "Holberton";
+	char *sh = "putchar";
 
 	while (*sh)
 	{
