d152ac6e20aef444a78feb1892f7e9031960b421
takudzwa94 <51080246+takudzwa94@users.noreply.github.com>
2022-09-13 12:55:10 +0200

    Update and rename holberton.h to main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 char _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
