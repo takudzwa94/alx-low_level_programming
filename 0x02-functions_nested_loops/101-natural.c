77c3706731d280f4ee01a79a46db64f2541b5c61
takudzwa94 <51080246+takudzwa94@users.noreply.github.com>
2022-09-14 15:57:26 +0200

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,6 +8,19 @@
  */
 int main(void)
 {
+	int i = 1;
+	int total = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+			total += i;
+		else if (i % 5 == 0)
+			total += i;
+
+		i++;
+	}
+	printf("%d\n", total);
 
 	return (0);
 }
