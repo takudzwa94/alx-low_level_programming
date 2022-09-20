947164f7b693d402d675d7a0d5addc232c7a48ba
takudzwa94 <51080246+takudzwa94@users.noreply.github.com>
2022-09-20 13:53:26 +0200

    Update and rename holberton.h to main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
